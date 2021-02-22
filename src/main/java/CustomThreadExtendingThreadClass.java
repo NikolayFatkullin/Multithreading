86ca2a593c7348b0289b48ce907ad774d682f51a
NikolayFatkullin <163amefeZ>
2021-02-22 16:50:42 +0200

    - Added logger to the project

--- src/main/java/CustomThreadExtendingThreadClass.java
+++ src/main/java/CustomThreadExtendingThreadClass.java
@@ -1,11 +1,14 @@
+import org.apache.log4j.Logger;
+
 public class CustomThreadExtendingThreadClass extends Thread {
     private int value;
-
+    private static final Logger logger = Logger.getLogger(CustomThreadExtendingThreadClass.class);
     @Override
     public void run() {
         for (int i = 0; i <= 100; i++) {
             value++;
             System.out.println(Thread.currentThread().getName() + " value = " + value);
+            logger.info(Thread.currentThread().getName() + " value = " + value);
         }
     }
 }
