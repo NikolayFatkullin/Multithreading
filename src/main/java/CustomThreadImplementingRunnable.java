41bf27413dcdddfa29cd4741026d9e05dc95b4df
NikolayFatkullin <163amefeZ>
2021-02-22 16:53:46 +0200

    - Checkstyle fixed

--- src/main/java/CustomThreadImplementingRunnable.java
+++ src/main/java/CustomThreadImplementingRunnable.java
@@ -2,13 +2,14 @@ import org.apache.log4j.Logger;
 
 public class CustomThreadImplementingRunnable implements Runnable {
     private int value;
-    private static final Logger logger = Logger.getLogger(CustomThreadImplementingRunnable.class);
+    private static final Logger LOGGER = Logger.getLogger(CustomThreadImplementingRunnable.class);
+
     @Override
     public void run() {
         for (int i = 0; i <= 100; i++) {
             value++;
             System.out.println(Thread.currentThread().getName() + " value = " + value);
-            logger.info(Thread.currentThread().getName() + " value = " + value);
+            LOGGER.info(Thread.currentThread().getName() + " value = " + value);
         }
     }
 }
